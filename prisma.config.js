52d96ad5f06ddbb4e84cbee2c473783ec28e1cd6
Елена <n.a.092024@yandex.ru>
2026-07-05 22:54:25 +0300

    fix: динамический url в prisma.config

--- prisma.config.js
+++ prisma.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from '@prisma/config'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
-const client = createClient({ url: 'file:./prisma/dev.db' })
+// Берем путь из переменной окружения Amvera, либо используем локальный
+const dbUrl = process.env.DATABASE_URL || 'file:./prisma/dev.db'
+
+const client = createClient({ url: dbUrl })
 const adapter = new PrismaLibSql(client)
 
 export default defineConfig({
@@ -12,6 +15,7 @@ export default defineConfig({
   },
   datasource: {
     provider: 'libsql',
+    url: dbUrl, // Передаем строку url для CLI Prisma, чтобы сборщик не падал
     adapter
   }
 })
